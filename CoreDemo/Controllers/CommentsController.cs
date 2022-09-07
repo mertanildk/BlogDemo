6af79c20d5dc4e40cf81935c6f77745f25f1486a
mertanildeke@gmail.com <mertanildeke@gmail.com>
2022-09-07 08:40:38 +0300

    29.Lesson PartialViewsComments

---
+++ CoreDemo/Controllers/CommentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class CommentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialAddCommand()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialCommandListByBlog()
+        {
+            return PartialView();
+        }
+    }
+}
