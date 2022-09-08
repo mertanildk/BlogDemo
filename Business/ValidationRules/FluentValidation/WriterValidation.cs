f9c6b9c93fc6bbc6aa0c09daf2431a7b7a861b71
mertanildeke@gmail.com <mertanildeke@gmail.com>
2022-09-08 19:54:43 +0300

    FluernValidation

---
+++ Business/ValidationRules/FluentValidation/WriterValidation.cs
@@ -0,0 +1,24 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class WriterValidation:AbstractValidator<Writer>
+    {
+        public WriterValidation()
+        {
+            RuleFor(writer => writer.Password).NotEmpty().WithMessage("Boş geçilemez");
+            //RuleFor(writer => writer.WriterName).NotEmpty().WithMessage("Boş geçilemez");
+            RuleFor(writer => writer.Mail).NotEmpty().WithMessage("Boş geçilemez");
+            RuleFor(writer => writer.WriterName).Matches(@"((?=.\d)(?=.[a-z])(?=.*[A-Z]).{6,})");
+        }
+ 
+    }
+}
