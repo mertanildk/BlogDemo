d435d7d909c8ecbe9f71f379a99f26cfdcec8070
mertanildeke@gmail.com <mertanildeke@gmail.com>
2022-09-13 13:07:59 +0300

    Dashboard. Blogs Detail And Update href

---
+++ CoreDemo/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcUI.ViewComponents.Blog
+{
+    public class BlogListDashboard:ViewComponent
+    {
+
+        IBlogService _blogService;
+
+        public BlogListDashboard(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_blogService.IncludeCategory());
+        }
+    }
+}
