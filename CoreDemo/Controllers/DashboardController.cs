f757c7de1cbef7faeaf04990fd63dbdd700d2cf2
mertanildeke@gmail.com <mertanildeke@gmail.com>
2022-09-14 10:06:45 +0300

    Dashboard Cards Info, GetAll.Count()

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcUI.Controllers
 {
     public class DashboardController : Controller
     {
+        ICategoryService _categoryService;
+        IBlogService _blogService;
+
+        public DashboardController(ICategoryService categoryService, IBlogService blogService)
+        {
+            _categoryService = categoryService;
+            _blogService = blogService;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.CategoryCount = _categoryService.GetAll().Count();
+            ViewBag.BlogCount = _blogService.GetAll().Count();
+            ViewBag.BlogCountByWriter = _blogService.GetAll().Where(x => x.WriterId == 3).Count();
             return View();
         }
     }
