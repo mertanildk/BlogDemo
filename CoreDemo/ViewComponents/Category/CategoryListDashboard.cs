ade409638017e48e4d072b3504f0589a7d8c38eb
mertanildeke@gmail.com <mertanildeke@gmail.com>
2022-09-14 08:30:03 +0300

    Dashboard , CategoryList For Dashboard

---
+++ CoreDemo/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,20 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcUI.ViewComponents.Category
+{
+    public class CategoryListDashboard:ViewComponent
+    {
+        ICategoryService _categoryService;
+
+        public CategoryListDashboard(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_categoryService.GetAll());
+        }
+    }
+}
