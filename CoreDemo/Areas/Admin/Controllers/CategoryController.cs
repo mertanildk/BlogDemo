012fd181f0c77f97e54d04e60f418b0755afc06d
mertanildeke@gmail.com <mertanildeke@gmail.com>
2022-09-18 12:12:48 +0300

    Admin Area

---
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        ICategoryService _categoryService;
+        
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_categoryService.GetAll());
+        }
+    }
+}
--- CoreDemo/Program.cs
+++ CoreDemo/Program.cs
@@ -66,8 +66,16 @@ app.UseRouting();
 app.UseAuthorization();
 
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Category}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.Run();
