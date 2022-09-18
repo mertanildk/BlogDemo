21e14d57561f7cc4b7fb7b34e62091ede412f343
mertanildeke@gmail.com <mertanildeke@gmail.com>
2022-09-18 11:01:25 +0300

    AdminPanel

---
+++ CoreDemo/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult AdmimNavbarPartial()
+        {
+            return PartialView();
+        }
+    }
+}
