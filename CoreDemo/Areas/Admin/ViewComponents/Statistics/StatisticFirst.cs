095a15283aa411497b1353a00e7cf2fd8071d338
mertanildeke@gmail.com <mertanildeke@gmail.com>
2022-09-21 20:28:49 +0300

    OpenWeather Degree

--- CoreDemo/Areas/Admin/ViewComponents/Statistics/StatisticFirst.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistics/StatisticFirst.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
 
 namespace MvcUI.Areas.Admin.ViewComponents.Statistics
 {
@@ -21,6 +23,15 @@ namespace MvcUI.Areas.Admin.ViewComponents.Statistics
             ViewBag.BlogCount = _blogService.GetAll().Count;
             ViewBag.MessageCount = _messageService.GetAll().Count;
             ViewBag.CommentCount = _commentService.GetAll().Count;
+
+            string apiKey = "55bae1403626b66bc18e2e5cd5af55ab";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=mersin&mode=xml&units=metric&appid=" + apiKey;
+            XDocument document = XDocument.Load(connection);
+
+            ViewBag.City = document.Descendants("city").Attributes("name").FirstOrDefault().Value;
+            var cityDegree = document.Descendants("temperature").Attributes("value").FirstOrDefault().Value;
+            ViewBag.Degree = cityDegree.Substring(0, cityDegree.IndexOf('.'));
+
             return View();
         }
     }
