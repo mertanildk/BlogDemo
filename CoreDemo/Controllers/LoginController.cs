7ce093907ead8a0d2b18878df1b1dff5855f9ab5
mertanildeke@gmail.com <mertanildeke@gmail.com>
2022-09-24 12:39:45 +0300

    Indetity devam

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,8 +1,11 @@
 ﻿using Business.Abstract;
+using Entities.Concrete;
 using Entity.Concrete;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MvcUI.Models;
 using System.Security.Claims;
 
 namespace MvcUI.Controllers
@@ -10,36 +13,50 @@ namespace MvcUI.Controllers
     [AllowAnonymous]
     public class LoginController : Controller
     {
-        IWriterService _writerService;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public LoginController(IWriterService writerService)
+        public LoginController(SignInManager<AppUser> signInManager)
         {
-            _writerService = writerService;
+            _signInManager = signInManager;
         }
-        
+
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> Index(Writer writer)
+        public async Task<IActionResult> Index(UserSignInViewModel appUser)
         {
-            var writerToFind= _writerService.GetAll()
-                .FirstOrDefault(x => x.Mail == writer.Mail && x.Password==writer.Password);
-
-            if (writerToFind is not null)
+            var result = await _signInManager.PasswordSignInAsync(appUser.UserName, appUser.Password, false, true);
+            if (result.Succeeded)
             {
-                var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name,writer.Mail)
-                };
-                var userIdentity = new ClaimsIdentity(claims, "login");//Neden 2.parametre olarak değer gönderiyoruz.
-                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
-                await HttpContext.SignInAsync(principal);
                 return RedirectToAction("Index", "Dashboard");
             }
-            return View();
+            return RedirectToAction("Index", "Login");
+
         }
+        
     }
 }
+
+
+//[HttpPost]
+//[AllowAnonymous]
+//public async Task<IActionResult> Index(Writer writer)
+//{
+//    var writerToFind = _writerService.GetAll()
+//        .FirstOrDefault(x => x.Mail == writer.Mail && x.Password == writer.Password);
+
+//    if (writerToFind is not null)
+//    {
+//        var claims = new List<Claim>
+//                {
+//                    new Claim(ClaimTypes.Name,writer.Mail)
+//                };
+//        var userIdentity = new ClaimsIdentity(claims, "login");//Neden 2.parametre olarak değer gönderiyoruz.
+//        ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+//        await HttpContext.SignInAsync(principal);
+//        return RedirectToAction("Index", "Dashboard");
+//    }
+//    return View();
+//}
---
+++ CoreDemo/Models/UserSignInViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MvcUI.Models
+{
+    public class UserSignInViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
