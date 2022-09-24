b05e5453b38d78f9d6c2faeed9ef0782b2be7b99
mertanildeke@gmail.com <mertanildeke@gmail.com>
2022-09-24 09:11:41 +0300

    IdentityKurulum

--- DataAccess/Concrete/EntityFramework/CoreBlogContext.cs
+++ DataAccess/Concrete/EntityFramework/CoreBlogContext.cs
@@ -1,15 +1,12 @@
 ﻿using Entities.Concrete;
 using Entity.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class CoreBlogContext:DbContext
+    public class CoreBlogContext:IdentityDbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
